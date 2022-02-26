d7c29733767b2bb8dd95821957becceb5b81ceff
Кузьмин Александр <kuzzminad@intersvyaz.net>
2022-02-26 13:23:06 +0500

    Коммит потому что Андрей попросил коммит

---
+++ scripts/resnet_train.py
@@ -0,0 +1,249 @@
+from enum import auto
+import numpy as np
+import torch
+import pandas as pd
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+import torchaudio
+import torchaudio.transforms as T
+from pathlib import Path
+from matplotlib import pyplot as plt
+import torch.optim as opt
+from tqdm import tqdm
+from torch import nn
+import torchvision.models as models
+import time
+
+MAX_AUDIO_LEN = 16000  # в отсчётах sr
+DEVICE = "cuda:0"
+BATCH_SIZE = 128
+N_EPOCHS = 100
+VAL_ITER = 10
+CLASSES = [
+    "down",
+    "go",
+    "left",
+    "no",
+    "off",
+    "on",
+    "right",
+    "stop",
+    "up",
+    "yes",
+]
+
+
+class MelCreator:
+    def __init__(self) -> None:
+        self.make_melspec = T.MelSpectrogram(
+            sample_rate=16000,
+            n_fft=1024,
+            win_length=1024,
+            hop_length=128,
+            f_min=55.0,
+            f_max=7600,
+            pad=0,
+            n_mels=128,
+            window_fn=torch.hann_window,
+            power=2.0,
+            normalized=True,
+            center=False,
+            pad_mode="reflect",
+            onesided=True,
+            norm="slaney",  #'slaney',
+            mel_scale="htk",
+        )
+        self.atdb = T.AmplitudeToDB(stype="power", top_db=80)
+        # self.atdb = T.AmplitudeToDB(stype="power", top_db=100)
+
+    def __call__(self, audio):
+        melspec = self.atdb(self.make_melspec(audio))
+        return melspec
+
+
+class TrainData(Dataset):
+    def __init__(self, audio_dir: Path, noise_dir: Path) -> None:
+        super().__init__()
+        self.audio_len = MAX_AUDIO_LEN
+        self.mel_creator = MelCreator()
+
+        self.audio_paths = list()
+        self.classes = list()
+        for class_dir in audio_dir.iterdir():
+            for wav_path in class_dir.iterdir():
+                audio, _ = torchaudio.load(wav_path)
+                self.audio_paths.append(audio)
+                self.classes.append(class_dir.name)
+
+        self.noises_paths = list()
+        for wav_path in noise_dir.iterdir():
+            noise, _ = torchaudio.load(wav_path)
+            self.noises_paths.append(noise)
+
+    def __len__(self):
+        return len(self.classes)
+
+    def __getitem__(self, idx):
+        noise_idx = np.random.randint(0, len(self.noises_paths) - 1)
+        noise = self.noises_paths[noise_idx]
+        # noise, _ = torchaudio.load(self.noises_paths[noise_idx])
+        if noise.shape[0] == 2:
+            noise = noise[np.random.randint(0, 2)]
+            noise = noise.unsqueeze(0)
+
+        audio = self.audio_paths[idx]
+        # audio, _ = torchaudio.load(self.audio_paths[idx])
+        audio = self.__pad_audio(audio)
+        audio = self.__add_noise(audio, noise, 0, 6)
+        audio = audio / audio.abs().max()
+
+        melspec = self.mel_creator(audio)
+
+        return melspec, CLASSES.index(self.classes[idx])
+
+    def __pad_audio(self, audio):
+        if self.audio_len - audio.shape[-1] > 0:
+            i = np.random.randint(0, self.audio_len - audio.shape[-1])
+        else:
+            i = 0
+        pad_patern = (i, self.audio_len - audio.shape[-1] - i)
+        audio = F.pad(audio, pad_patern, "constant").detach()
+        return audio
+
+    def __add_noise(self, clean, noise, min_amp, max_amp):
+        noise_amp = np.random.uniform(min_amp, max_amp)
+        # так как шумная запись длиннее, то выбираем случайный момент начала шумной записи
+        start = np.random.randint(0, noise.shape[1] - clean.shape[1] + 1)
+        noise_part = noise[:, start : start + clean.shape[1]]
+
+        # накладываем шум
+        noise_mult = clean.abs().max() / noise_part.abs().max() * noise_amp
+        return (clean + noise_part * noise_mult) / (1 + noise_amp)
+
+
+class TestData(Dataset):
+    def __init__(self, audio_dir: Path, markup_path) -> None:
+        super().__init__()
+        self.audio_len = MAX_AUDIO_LEN
+        self.mel_creator = MelCreator()
+
+        self.audio_paths = list()
+        self.classes = list()
+        markup = pd.read_csv(markup_path)
+        for file_name, category in markup.values:
+            audio, _ = torchaudio.load(audio_dir / f"{file_name}.wav")
+            self.audio_paths.append(audio)
+            self.classes.append(category)
+
+    def __len__(self):
+        return len(self.classes)
+
+    def __getitem__(self, idx):
+        audio = self.audio_paths[idx]
+        audio = self.__pad_audio(audio)
+        audio = audio / audio.abs().max()
+        melspec = self.mel_creator(audio)
+
+        return melspec, CLASSES.index(self.classes[idx])
+
+    def __pad_audio(self, audio):
+        if self.audio_len - audio.shape[-1] > 0:
+            i = np.random.randint(0, self.audio_len - audio.shape[-1])
+        else:
+            i = 0
+        pad_patern = (i, self.audio_len - audio.shape[-1] - i)
+        audio = F.pad(audio, pad_patern, "constant").detach()
+
+        return audio
+
+
+def load_model():
+    # model = torch.hub.load(
+    #     "NVIDIA/DeepLearningExamples:torchhub",
+    #     "nvidia_efficientnet_b0",
+    #     pretrained=False,
+    # )
+    # model.stem.conv = nn.Conv2d(
+    #     1, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False
+    # )
+    # num_ftrs = model.classifier.fc.in_features
+    # model.classifier.fc = nn.Linear(num_ftrs, 10, bias=True)
+
+    model = models.resnet18()
+    # print(model)
+    model.conv1 = nn.Conv2d(
+        1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False
+    )
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, 10, bias=True)
+
+    return model
+
+
+if __name__ == "__main__":
+    torch.autograd.set_detect_anomaly(True)
+
+    noise_dir = Path("hackaton_ds/noises")
+    train_dir = Path("hackaton_ds/train")
+    train = TrainData(train_dir, noise_dir)
+    train_loader = DataLoader(train, batch_size=BATCH_SIZE, shuffle=True, num_workers=6)
+
+    test_dir = Path("hackaton_ds/test")
+    test_markup = Path("hackaton_ds/submission_xvector_cos_sim.csv")
+    test = TestData(test_dir, test_markup)
+    val_loader = DataLoader(test, batch_size=BATCH_SIZE, shuffle=False, num_workers=6)
+
+    model = load_model().to(DEVICE)
+    optimizer = opt.AdamW(model.parameters(), lr=0.001)
+
+    train_losses, train_accs = [], []
+    val_losses, val_accs = [], []
+    for epoch in range(N_EPOCHS):
+        start = time.time()
+        model.train()
+
+        train_acc = list()
+        train_loss = list()
+        for X, Y in tqdm(train_loader):
+            preds = model.forward(X.to(DEVICE))
+            # print(Y)
+            loss = F.cross_entropy(
+                preds,
+                Y.to(DEVICE),
+            )
+            print(loss)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_loss.append(loss.cpu().data.numpy())
+            train_acc.extend((torch.argmax(preds, dim=-1).cpu() == Y).data.numpy())
+            # print(train_loss)
+
+        if epoch % VAL_ITER == 0:
+            model.eval()
+
+            val_acc = list()
+            val_loss = list()
+            for X, Y in tqdm(val_loader):
+                preds = model.forward(X.to(DEVICE))
+                loss = F.cross_entropy(
+                    preds,
+                    Y.to(DEVICE),
+                )
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                val_loss.append(loss.cpu().data.numpy())
+                val_acc.extend((torch.argmax(preds, dim=-1).cpu() == Y).data.numpy())
+                torch.save(model.state_dict(), f"models/efficientnet_{epoch}ep.pt")
+
+        epoch_time = time.time() - start
+        train_acc = sum(train_acc) / len(train_acc)
+        train_loss = sum(train_loss) / len(train_loss)
+        val_acc = sum(val_acc) / len(val_acc)
+        val_loss = sum(val_loss) / len(val_loss)
+        print(
+            f"Epoch time: {epoch_time:.2}|Train acc: {train_acc:.2}|Train loss: {train_loss:.2}|Val acc: {val_acc:.2}|Val loss: {val_loss:.2}"
+        )
